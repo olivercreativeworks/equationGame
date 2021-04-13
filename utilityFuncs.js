aeb484a9c9fc0ffb770dc2abcbb1e68990620405
Oliver_Windows <olivercreativeworks@gmail.com>
2021-04-12 20:39:26 -0400

    Refactored Create Equation Function

--- utilityFuncs.js
+++ utilityFuncs.js
@@ -1,7 +1,74 @@
+"use strict";
 // Utility Functions
+/**
+ * Returns a random key from an array or dictonary
+ * @param {object} options The array or dictionary you want to get a random key from
+ * @return {string | number} A random key from the array or dictionary
+ */
 function randomChoice(options) {
-    var choices = Object.keys(options);
-    var randomIndex = Math.floor(Math.random() * choices.length);
-    var randomChoice = choices[randomIndex];
-    return randomChoice;
+    var optionsKeysList = Object.keys(options);
+    var randomIndex = Math.floor(Math.random() * optionsKeysList.length);
+    var randomKey = optionsKeysList[randomIndex];
+    return options[randomKey];
 }
+function createEquation(listOfTerms, numberOfTerms, preferredFirstTerms) {
+    if (typeof (numberOfTerms) !== 'number') {
+        throw TypeError("@param numberOfTerms should be type number, not " + typeof (numberOfTerms));
+    }
+    if (numberOfTerms < 1) {
+        throw RangeError('@param numberOfTerms cannot be less than 1');
+    }
+    var equation = [];
+    if (preferredFirstTerms) {
+        var firstTerm = randomChoice(preferredFirstTerms);
+        equation.push(firstTerm);
+    }
+    while (equation.length < numberOfTerms) {
+        var nextTerm = randomChoice(listOfTerms);
+        equation.push(nextTerm);
+    }
+    return equation;
+}
+function dataIsArray(data) {
+    if (Array.isArray(data)) {
+        return true;
+    }
+}
+function showKeys(obj) {
+    var keys = Object.keys(obj);
+    return keys;
+}
+function showValues(obj) {
+    var keys = Object.keys(obj);
+    var values = [];
+    for (var key in keys) {
+        values.push(obj[key]);
+    }
+    return values;
+}
+function showItems(obj) {
+    var keys = showKeys(obj);
+    var values = showValues(obj);
+    var items = new Object();
+    for (var i = 0; i < keys.length; i++) {
+        items[keys[i]] = values[i];
+    }
+    return JSON.stringify(items);
+}
+var operations = {
+    '-': '-',
+    '+': '+',
+    '': ' '
+};
+var colors = {
+    0: 'mint',
+    1: 'red',
+    2: 'yellow',
+    3: 'blue',
+    4: 'green',
+    5: 'orange',
+    6: 'purple',
+    7: 'black',
+    8: 'white',
+    9: 'grey',
+};
--- utilityFuncs.ts
+++ utilityFuncs.ts
@@ -9,7 +9,80 @@ function randomChoice(options:object) {
  const optionsKeysList = Object.keys(options)
  const randomIndex = Math.floor(Math.random() * optionsKeysList.length)
  const randomKey = optionsKeysList[randomIndex]
- return randomKey
+ return options[randomKey]
 }
+
+function createEquation( listOfTerms:object, numberOfTerms:number, preferredFirstTerms?:object) {
+ if (typeof (numberOfTerms) !== 'number') {
+  throw TypeError(`@param numberOfTerms should be type number, not ${typeof(numberOfTerms)}`)
+ }
+ if (numberOfTerms < 1) {
+  throw RangeError('@param numberOfTerms cannot be less than 1')
+ }
+ let equation = [];
+ if (preferredFirstTerms) {
+  const firstTerm = randomChoice(preferredFirstTerms)
+  equation.push(firstTerm)
+  }
+ while (equation.length < numberOfTerms) {
+  const nextTerm = randomChoice(listOfTerms)
+  equation.push(nextTerm)
+ }
+ return equation
+}
+
+function dataIsArray(data:any) {
+ if (Array.isArray(data)) {
+  return true
+ }
+}
+
+function showKeys(obj:any) {
+ const keys = Object.keys(obj)
+ return keys
+}
+
+function showValues(obj:any) {
+ const keys = Object.keys(obj)
+ let values = []
+ for (let key in keys) {
+  values.push(obj[key])
+ }
+ return values
+}
+
+function showItems(obj: any) {
+ let keys = showKeys(obj)
+ let values = showValues(obj)
+ let items = new Object();
+ for (let i = 0; i < keys.length; i++) {
+  items[keys[i]] = values[i]
+ }
+ return JSON.stringify(items)
+}
+
+const operations: object = {
+ '-': '-',
+ '+': '+',
+ '':' '
+}
+
+const colors:object = {
+ 0: 'mint',
+ 1: 'red',
+ 2 : 'yellow',
+ 3 : 'blue',
+ 4 : 'green',
+ 5 : 'orange',
+ 6 : 'purple',
+ 7 : 'black',
+ 8 : 'white',
+ 9 : 'grey',
+}
+
+
+
+
+
 
 
